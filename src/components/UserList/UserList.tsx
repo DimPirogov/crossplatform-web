344d9ec099439bf372d221c02e1d053ef199f590
Dimitri <dimpirogovp@hotmail.com>
2023-10-31 14:12:05 +0100

    added delete

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,19 +1,25 @@
 import styles from "./UserList.module.css";
-import { useGetUsersQuery } from "../../store/api/usersApi";
+import { useGetUsersQuery,
+          useDeleteUserMutation } from "../../store/api/usersApi";
 
 export const UserList = () => {
   const { data, refetch } = useGetUsersQuery();
-  // const reload = () => {
-  //   forceUpdate();
-  // }
-  console.log(data);
+  const [ deleteUser ] = useDeleteUserMutation();
+  var nr = 0;
+  //console.log(data);
 
   return (
     <div className={styles.container}>
       <p>User list!</p>
       <ul className={styles.ul}>
-        { !data ? 'loading' : data.map((user) => {
-            return <li>{user.firstName} {user.lastName }</li>
+        { !data ? <li>'loading'</li> : data.map((user) => { nr++;
+            return <li key={user.id}>
+                    {nr}: {user.firstName} {user.lastName }
+                    <button
+                      className={styles.deleteButton}
+                      onClick={() => deleteUser(user.id)}
+                    >radera</button>
+                  </li>
           })
         }
       </ul>
--- src/store/api/usersApi.ts
+++ src/store/api/usersApi.ts
@@ -1,6 +1,6 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { db } from '../../firebase-config';
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 
 const firebaseBaseQuery = async ({ baseUrl, url, method, body }) => {
     switch (method) {
@@ -12,6 +12,10 @@ const firebaseBaseQuery = async ({ baseUrl, url, method, body }) => {
         case 'POST':
             const docRef = await addDoc(collection(db, url), body);
             return { data: { id: docRef.id, ...body } };
+
+        case 'DELETE':
+            await deleteDoc(doc(db, url, body));
+            return { data: `Deleted id ${body}`};
 
         default:
             throw new Error(`Unhandled method ${method}`);
@@ -40,7 +44,19 @@ export const usersApi = createApi({
           body: '',
         }),
       }),
+      deleteUser: builder.mutation({
+        query: ( id ) => ({
+          baseUrl: '',
+            url: 'users',
+            method: 'DELETE',
+            body: id
+        })
+      })
     }),
 });
 
-export const { useCreateUserMutation, useGetUsersQuery } = usersApi;
+export const {
+              useCreateUserMutation,
+              useGetUsersQuery,
+              useDeleteUserMutation
+            } = usersApi;
